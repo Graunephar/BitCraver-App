60dd3c4d78b20a0c24f70dc604f7a5b7d0aa71b0
Daniel Graungaard <graungaard@cs,au.dk>
2020-08-31 16:55:28 +0200

    Processign HTML in webview

---
+++ app/src/main/java/com/example/bitcraver/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.bitcraver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.text.Html;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView view = findViewById(R.id.content_view);
+
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading Data...");
+        progressDialog.setCancelable(false);
+
+        WebView web_view = findViewById(R.id.content_view);
+
+        web_view.requestFocus();
+        web_view.getSettings().setLightTouchEnabled(true);
+        web_view.getSettings().setJavaScriptEnabled(true);
+        web_view.getSettings().setGeolocationEnabled(true);
+        web_view.setSoundEffectsEnabled(true);
+        web_view.loadData("",
+                "text/html", "UTF-8");
+
+        web_view.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress) {
+                if (progress < 100) {
+                    progressDialog.show();
+                }
+                if (progress == 100) {
+                    progressDialog.dismiss();
+                }
+            }
+        });
+
+
+    }
+}
