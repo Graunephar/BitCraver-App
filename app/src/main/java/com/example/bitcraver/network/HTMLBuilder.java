3641d1362a5b584190852049788ac67691279c5c
Daniel Graungaard <graungaard@cs,au.dk>
2020-09-01 15:18:35 +0200

    Fixed image bug

--- app/src/main/java/com/example/bitcraver/network/HTMLBuilder.java
+++ app/src/main/java/com/example/bitcraver/network/HTMLBuilder.java
@@ -5,7 +5,19 @@ import android.text.Html;
 public class HTMLBuilder {
 
 
+    public static String removeUnWantedTags(String source) {
+
+        source = source.replace("loading=\"lazy\"", "");
+
+        source = source.replace("http://", "https://"); //http can be dissallowed on newer phones
+
+        return source;
+    }
+
+
     public static String StringtoHTML(String content) {
+
+        content = removeUnWantedTags(content);
 
         return "<!DOCTYPE html>\n" +
                 "<html lang=\"en\">\n" +
